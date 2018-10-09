0e56253df639a1dc93e92d2c253ab8ccfa9430de
rsajjan <32497743+rsajjan3@users.noreply.github.com>
2018-10-09 08:49:05 -0400

    Add first files

---
+++ BracketBalancer/BracketBalancer/MyStack.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketBalancer
+{
+    class MyStack
+    {
+        private char[] elements;
+        private int top_index;
+        private int max_size;
+
+        public MyStack(int size)
+        {
+            this.elements = new char[size];
+            this.top_index = -1; //Currently no top, so set some arbitrary value
+            this.max_size = size;
+        }
+        public int length
+        {
+            get
+            {
+                return top_index + 1;
+            }
+        }
+
+        public void push (char item)
+        {
+            if (this.top_index == this.max_size - 1) { throw new StackOverflowException("Stack is full"); }
+            else
+            {
+                this.top_index++; //Increase the counter so there is a new 'top'
+                this.elements[this.top_index] = item; //Add item to the 'top' of the stack
+            }
+        }
+
+        public void mass_push(String items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                this.push(items[i]);
+            }
+        }
+        public void mass_push(char[] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                this.push(items[i]);
+            }
+        }
+
+        public char pop()
+        {
+            if (this.top_index == -1) { throw new StackOverflowException("Trying to remove from an empty stack"); }
+            else
+            {
+                return this.elements[this.top_index--]; //Return the top item and move the 'top' down one index
+            }
+        }
+
+        public char peek()
+        {
+            if (this.top_index == -1) { throw new StackOverflowException("Trying to access an empty stack"); }
+            else
+            {
+                return this.elements[this.top_index]; //Return the top item
+            }
+        }
+
+        public bool balanced()
+        {
+            if ((this.length) % 2 != 0) return false; //Has to be an even numbr of elements to be balanced.
+            else
+            {
+                MyStack tmp_stack = new MyStack(this.top_index);
+                foreach(char item in this.elements)
+                {
+                    if (item == '{' || item == '[' || item == '(')
+                    {
+                        tmp_stack.push(item);
+                    }
+                    else if (tmp_stack.length > 0)
+                    {
+                        if (item == '}')
+                        {
+                            if (tmp_stack.peek() == '{') tmp_stack.pop();
+                            else return false; //Brackets aren't matching return false asap
+                        }
+                        else if (item == ']')
+                        {
+                            if (tmp_stack.peek() == '[') tmp_stack.pop();
+                            else return false;
+                        }
+                        else if (item == ')')
+                        {
+                            if (tmp_stack.peek() == '(') tmp_stack.pop();
+                            else return false;
+                        }
+                        else
+                        {
+                            return false; //Non-bracket input
+                        }
+                    }
+                    else return false;
+                }
+                if (tmp_stack.length > 0) return false; //If there are still items then it is not balanced
+                else return true;
+            }
+        }
+
+        public void print_stack()
+        {
+            for(int i = this.top_index; i >= 0; i--)
+            {
+                Console.WriteLine(this.elements[i]);
+            }
+        }
+    }
+}
---
+++ BracketBalancer/BracketBalancer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketBalancer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sample strings to try:
+            // {([])[()]}
+            // (([][){})
+            // {[[[]]]}
+            // {[[({})]]}
+            MyStack steck;
+            String[] test_cases = new String[] { "{([])[()]}", "(([][){})", "{[[[]]]}", "{[[({})]]}", "{{{{{{{{{", "{{{{}}" };
+
+            for (int i = 0; i < test_cases.Length; i++)
+            {
+                Console.WriteLine("Test Case: " + test_cases[i]);
+                steck = new MyStack(test_cases[i].Length);
+                steck.mass_push(test_cases[i]);
+                PrintContents(steck);
+                PrintBalance(steck);
+            }
+
+            while (true)
+            {
+                Console.Write("Enter in your test string(no spaces): ");
+                String user_input = Console.ReadLine();
+
+                steck = new MyStack(user_input.Length);
+                steck.mass_push(user_input);
+
+                PrintContents(steck);
+
+                PrintBalance(steck);
+
+                Console.ReadLine();
+            }
+        }
+        static void PrintContents(MyStack input)
+        {
+            Console.WriteLine("STACK CONTENTS: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            input.print_stack();
+            Console.ResetColor();
+            Console.WriteLine("END STACK CONTENTS\r\n");
+        }
+        static void PrintBalance(MyStack input)
+        {
+            bool balance = input.balanced();
+            if (balance) Console.WriteLine("String is balanced\r\n");
+            else Console.WriteLine("String is not balanced\r\n");
+        }
+    }
+}
