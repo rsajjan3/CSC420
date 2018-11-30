9e773da63254c29f86fb664a415f4acc86525480
rsajjan <32497743+rsajjan3@users.noreply.github.com>
2018-11-30 12:13:20 -0500

    Update HW05

--- HW5_Hashing/HW5_Hashing/Program.cs
+++ HW5_Hashing/HW5_Hashing/Program.cs
@@ -11,15 +11,30 @@ namespace HW5_Hashing
     {
         static void Main(string[] args)
         {
+            int[] indicies = new int[25];
+            MyHashTable tbl = new MyHashTable(50);
             Random ran = new Random();
-            MyHashTable tbl = new MyHashTable(35);
-
-            for (int i = 0; i < 25; i++)
+            for (int x = 0; x < 40; x++)
             {
-                tbl.add(ran.Next(0, 400), ran.Next(10000, 100000));
+                tbl = new MyHashTable(50);
+                for (int i = 0; i < 25; i++)
+                {
+                    indicies[i] = ran.Next(0, 200);
+                    tbl.add(indicies[i], ran.Next(10000, 100000));
+                }
+                Console.WriteLine("Run ({0}/{1}). Average Add Probes: {2}", (x + 1), 40, tbl.AverageAddProbes);
+            }
+            Console.WriteLine();
+
+
+            for (int x = 0; x < 40; x++) //Only tests the last hash table created above.
+            {
+                int idx = indicies[ran.Next(0, indicies.Length)];
+                tbl.find(idx);
+                Console.WriteLine("Run ({0}/{1}). Average Find Probes: {2}", (x + 1), 40, tbl.AverageFindProbes);
             }
 
-            Console.WriteLine(tbl.AverageAddProbes);
+            Console.WriteLine(tbl.find(indicies[0]));
             Console.ReadLine();
         }
     }
